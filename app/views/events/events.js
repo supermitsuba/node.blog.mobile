e8ca5f77dedfb4c544f47cc6ea1dd49ae06ed9f7
Jorden Lowe <supermitsuba@gmail.com>
2016-01-24 21:00:03 -0500

    Added IOS code to visit a link

--- app/views/events/events.js
+++ app/views/events/events.js
@@ -4,6 +4,7 @@ var observableModule = require("data/observable");
 var pageData = new observableModule.Observable();
 var ios = require("../../shared/iosPageLoad.js");
 var list = require("../../shared/displayList.js");
+var application = require("application");
 
 exports.eventPageLoaded = function (args) {
 		var page = args.object;
@@ -15,3 +16,11 @@ exports.eventPageLoaded = function (args) {
     var url = "http://www.jordenlowe.com/api/events";
 		list.LoadList(url, events);
 };
+
+exports.expandDetail = function(args) {
+	var item = args.view;
+	var index = args.index;
+	var link = events.getItem(args.index).WebLink;
+	var app = application.ios.nativeApp;
+	app.openURL(NSURL.URLWithString(link));
+}
